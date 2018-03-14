e29d28c1ce586529ba34d74c63d1c1de405b3e2f
谢汝 <xieru@qiyi.com>
2018-03-14 13:34:53 +0800

    up

---
+++ test.js
@@ -0,0 +1,17 @@
+'use strict'
+var Koa = require('koa');
+var sha1 = require('sha1');
+var config = {
+    wechat:{
+
+    }
+};
+
+var app = new Koa();
+
+app.use(function *(next){
+    console.log(this.query);
+})
+
+app.listen(1234);
+console.log('listening : 1234');
