c4bba22fa203f2e6df534a57ea420d34f42121f2
谢汝 <xieru@qiyi.com>
2018-03-14 16:08:32 +0800

    access_token

--- test.js
+++ test.js
@@ -2,11 +2,22 @@
 var Koa = require('koa');
 var sha1 = require('sha1');
 var wechat = require('./wechat/g');
+var path = require('path');
+var util = require('./libs/util')
+
+var wechat_file = path.join(__dirname,'/wechat/config/wechat.txt')
 var config = {
     wechat:{
         appID: "wxe5170cac100b8e27",
         appSecret: "8e3b900c3a4ae2ec8eeb104341764fd1",
-        token: 'ryantestasxxxxxtoke'
+        token: 'ryantestasxxxxxtoke',
+        getAccessToken:function(){
+           return util.readFileAsync(wechat_file)
+        },
+        saveAccessToken:function(data){
+            data = JSON.stringify(data);
+            return util.writeFileAsync(wechat_file)
+        }
     }
 };
 
