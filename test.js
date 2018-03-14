f56ce9ff5fc58468c0ce82fb1f25861b19992128
谢汝 <xieru@qiyi.com>
2018-03-14 14:05:45 +0800

    up

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ var Koa = require('koa');
 var sha1 = require('sha1');
 var config = {
     wechat:{
-
+        appID: "wxe5170cac100b8e27",
+        appSecret: "8e3b900c3a4ae2ec8eeb104341764fd1",
+        token: 'ryantestasxxxxxtoke'
     }
 };
 
@@ -11,6 +13,21 @@ var app = new Koa();
 
 app.use(function *(next){
     console.log(this.query);
+
+    var token = config.wechat.token;
+    var signature = this.query.signature;
+    var nonce = this.query.nonce;
+    var timestamp = this.query.timestamp;
+    var echostr = this.query.echostr;
+    var str = [token,timestamp,nonce].sort().join('');
+    var sha = sha1(str);
+
+    if(sha === signature) {
+        this.body = echostr + '';
+    }else {
+        this.body = 'wrong'
+    }
+
 })
 
 app.listen(80);
