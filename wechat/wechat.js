a58f262fe0a7eb9dfbfd8270d3ab486aa4afa6bc
谢汝 <xieru@qiyi.com>
2018-03-15 11:00:44 +0800

    no message

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -78,7 +78,7 @@ Wechat.prototype.updateAccessToken = function() {
 
 Wechat.prototype.reply = function (){
     var content = this.body;
-    var message = thi.weixin;
+    var message = this.weixin;
 
     var xml = util.tpl(content,message);
     this.status = 200;
